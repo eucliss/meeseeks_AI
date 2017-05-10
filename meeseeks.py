87f34acefa5cec3afad5d554f0bdb254c9962fae
Miller Kinlin <millerkinlin@gmail.com>
2017-05-10 00:48:51 -0700

    remove merge stuff

--- meeseeks.py
+++ meeseeks.py
@@ -7,15 +7,9 @@
 import pygame as pg
 import speech_recognition as sr
 import subprocess
-<<<<<<< Updated upstream
 import spotipy
 import spotipy.util as util
 import sys
-=======
-import time
-from forecastiopy import *
-
->>>>>>> Stashed changes
 
 def play_music(music_file, volume=0.8):
     '''
@@ -66,7 +60,7 @@ def recordAudio():
 
 def main():
     # Greetingz
-    # play_music("lookatme.mp3", 1)
+    play_music("lookatme.mp3", 1)
 
     dats = recordAudio()
     #print(dats)
@@ -80,7 +74,7 @@ def main():
         sp = spotipy.Spotify() # Instance of spotipy
 
         # When searching for a song to play
-        uri = False
+        uri = False # Initialize uri so the conditional below in line 85 doesnt throw erro
         if len(dat_words) > 2:
             search = sp.search(q=" ".join(dat_words[2:]))
             if search:
@@ -106,63 +100,9 @@ def main():
         main("play Spotify")
 
 
-<<<<<<< Updated upstream
 if __name__ == "__main__":
     main()
-=======
-# if __name__ == "__main__":
-#     print(" hello ")
-#     main()
 
-
-
-# this is called from the background thread
-def callback(recognizer, audio):
-    # received audio data, now we'll recognize it using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        data = recognizer.recognize_google(audio)
-        print("Google Speech Recognition thinks you said " + data)
-        checkData(data)
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-def checkData(data):
-    print(data)
-    if data == "play Spotify":
-        music_file = "cando.mp3"
-        volume = 1
-        play_music(music_file, volume)
-        subprocess.check_output(['spotify','play'])
-
-def spotify(data):
-    result = {
-      'a': lambda x: x * 5,
-      'b': lambda x: x + 7,
-      'c': lambda x: x - 2
-    }['a'](3)
-
-
-r = sr.Recognizer()
-m = sr.Microphone()
-with m as source:
-    r.adjust_for_ambient_noise(source)  # we only need to calibrate once, before we start listening
-
-# start listening in the background (note that we don't have to do this inside a `with` statement)
-stop_listening = r.listen_in_background(m, callback)
-print(stop_listening)
-# `stop_listening` is now a function that, when called, stops background listening
->>>>>>> Stashed changes
-
-# do some other computation for 5 seconds, then stop listening and keep doing other computations
-for _ in range(10): time.sleep(0.1)  # we're still listening even though the main thread is doing other things
-# for _ in range(50): time.sleep(0.1)  # we're still listening even though the main thread is doing other things
-# stop_listening()  # calling this function requests that the background listener stop listening
-while True: time.sleep(0.1)
 
 
 # r = sr.Recognizer()
